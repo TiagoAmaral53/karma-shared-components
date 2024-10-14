f7915a6523ae9fa58f9419c282069f928365cd4c
Tiago Amaral <tiago.amaral1990@gmail.com>
2024-10-14 14:14:57 +0100

    update

--- karma-components-playground/src/App.tsx
+++ karma-components-playground/src/App.tsx
@@ -2,7 +2,7 @@ import './App.css';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline bg-blue-600">Hello world!</h1>
+    <h1 className="text-3xl font-bold underline">Hello world!</h1>
   );
 }
 
