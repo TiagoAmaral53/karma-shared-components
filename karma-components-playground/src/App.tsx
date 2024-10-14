5ebe74a36eee8749f08a2f52e30274fecd48e53b
Tiago Amaral <tiago.amaral1990@gmail.com>
2024-10-14 18:57:40 +0100

    add footer and header in the playgroung

--- karma-components-playground/src/App.tsx
+++ karma-components-playground/src/App.tsx
@@ -1,12 +1,16 @@
 import './App.css';
-import { Atom, Button } from '../../dist/esm';
+import { Atom, Button, Footer, Header } from '../../dist/esm';
 
 function App() {
   return (
     <>
-      <h1 className="">Hello world!</h1>
-      <Atom label={'Teste'}></Atom>
-      <Button label={'click'} onClick={() => alert('hello world')}></Button>
+      <Header title={'React com vite'} ></Header>
+      <div id='app'>
+        <h1 className="">Hello world!</h1>
+        <Atom label={'Teste'}></Atom>
+        <Button label={'click'} onClick={() => alert('hello world')}></Button>
+      </div>
+      <Footer links={[{ name: "test", url: "" }]} ></Footer >
     </>
   );
 }
