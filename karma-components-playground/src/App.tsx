973b78ebb6e0c272a864d8ebc63af343216840de
Tiago Amaral <tiago.amaral1990@gmail.com>
2024-10-14 14:39:04 +0100

    add components to the vite app

--- karma-components-playground/src/App.tsx
+++ karma-components-playground/src/App.tsx
@@ -1,7 +1,14 @@
 import './App.css';
+import { Atom, Button } from '../../dist/esm';
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Atom label={'Teste'}></Atom>
+      <Button label={'click'} onClick={() => alert('hello world')}></Button>
+    </>
+  );
 }
 
 export default App;
