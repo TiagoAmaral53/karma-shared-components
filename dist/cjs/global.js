4604300e9ef86c9af56aa4f4da32fb0914a23cac
Tiago Amaral <tiago.amaral1990@gmail.com>
2024-10-17 21:07:21 +0100

    test new dist

--- dist/cjs/global.js
+++ dist/cjs/global.js
@@ -1,2 +1,2 @@
-"use strict";module.exports=undefined;
+"use strict";var e=require("./chunks/chunck.js");require("./chunks/vendor.js"),require("react"),require("react/jsx-runtime"),require("./chunks/atoms.js"),require("./chunks/ui.js"),require("./chunks/molecules.js"),require("./chunks/organisms.js"),require("./chunks/pages.js"),require("./chunks/templates.js"),module.exports=e.undefined;
 //# sourceMappingURL=global.js.map
